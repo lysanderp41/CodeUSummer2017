cab86baf97b1c73218c18a1122161bdc9efffa93
lysanderP <lysanderp41@gmail.com>
2017-06-27 00:39:39 -0400

    Merge branch 'make-status-update' of https://github.com/lysanderp41/CodeUSummer2017 into Persistent-Storage for access to status-update code.

--- src/codeu/chat/server/Server.java
+++ src/codeu/chat/server/Server.java
@@ -15,30 +15,19 @@
 
 package codeu.chat.server;
 
+import codeu.chat.common.*;
+import codeu.chat.util.*;
+import codeu.chat.util.connections.Connection;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
-
-import codeu.chat.common.ServerInfo;
-import codeu.chat.common.ConversationHeader;
-import codeu.chat.common.ConversationPayload;
-import codeu.chat.common.Interests;
-import codeu.chat.common.LinearUuidGenerator;
-import codeu.chat.common.Message;
-import codeu.chat.common.NetworkCode;
-import codeu.chat.common.Relay;
-import codeu.chat.common.Secret;
-import codeu.chat.common.User;
-import codeu.chat.util.*;
-import codeu.chat.util.connections.Connection;
+import java.util.Scanner;
 
 public final class Server {
 
